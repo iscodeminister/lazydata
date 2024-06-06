b86a83108da276ef92e43abef5ebf44f0e0363d3
Ting <50560278+iscodeminister@users.noreply.github.com>
2024-06-06 17:03:10 +0000

    zzzz

--- install.sh
+++ install.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
 display_menu() {
-  echo "Please choose a file to download from GitHub:"
-  echo "1) File 1"
+  echo "Please choose your destiny:"
+  echo "1) one key MLlib"
   echo "2) Exit"
 }
 
 download_file() {
   local file_url="$1"
   local file_name=$(basename "$file_url")
-
   echo "Downloading $file_name..."
-  
   wget "$file_url"
-  
   echo "Downloaded $file_name"
 }
 
 GITHUB_BASE_URL="https://raw.githubusercontent.com/iscodeminister/lazydata/main"
 
-FILE1_URL="$GITHUB_BASE_URL/path/to/file1"
-
 while true; do
   display_menu
-  
   read -p "Enter your choice: " choice
-  
   case $choice in
     1)
-      download_file "$FILE1_URL"
+      mkdir mllib
+      cd mllib
+      hdfs dfs -mkdir -p /data
+      download_file "$GITHUB_BASE_URL/mllib/test.csv"
+      download_file "$GITHUB_BASE_URL/mllib/train.csv"
+      hdfs dfs -put * /data
+      cd ~
+      download_file "$GITHUB_BASE_URL/pipeline.py"
+      spark-submit --master local pipeline.py
       ;;
     2)
       echo "Exiting..."
